f445eaaa02b992e7003780e0a78a50702e20bb62
hugo valois <hvgaming@yahoo.com>
2018-08-09 21:32:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@
 
 
 def position_taken?(board,index)
- if board[index] == " "  && board[index] == ""
+ if board[index] == " "  || board[index] == ""
    return false 
     elsif index == " " && ""
    return nil
